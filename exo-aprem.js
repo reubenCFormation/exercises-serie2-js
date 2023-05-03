abef9f0745e47832f48588e8f0420f5b692a877c
yunities-front <reubenCFormation@gmail.com>
2023-05-03 19:26:17 +0200

    exercises-serie-2

---
+++ exo-aprem.js
@@ -0,0 +1,11 @@
+// Creer une fonction appelé squareNumber qui va prendre un numero en parmetre. Retourne le carré de ce numero et stocker le resulat dans une variable 
+
+//2)Creer une fonction appelé testStrLength qui prend un tableau comme parametre. Cette fonction testera si la longuer de la chaine stocké dans le tableau sera  supperieur a 5.  On va ensuite (avec la creation d'une variable), stocker le nombre de chaines de caracteres qui ont une longeur supperieur a 5 et on va devoir acceder a cette information en dehors de notre fonction
+var stringArray=["Pizza","Steak","Burger","oeuf mayo","Cantal","Emmental","Vin"];
+
+// BONUS-------------------------------------------------
+
+//3) Creer une fonction qui prendra comme parametre un numero entre 0 et 50. Si le numero est superieur a 25 retourner true, sinon retourner false. Dans UNE AUTRE fonction, trouver un moyen de generer un numero aleatoire entre 0 et 50 20 fois et a chaque fois, faites appel a la premeire fonction pour determiner si le numero est superiuer a 25 ou pas. Ensuite stocker le nombre d'occurences avec un nuemro supperieur a 25 dans une autre variable!
+
+// 4 (au lieu de repeter 20 fois, le nombre de fois a repeter sera encore un parametre ente 20 et 100!)
+
---
+++ exo-matin.js
@@ -0,0 +1,17 @@
+//1) Affichez, via un console.log, chaque elemnt de notre tableau de maniere dynamique (hint: Une boucle!)
+ var myArray=["first","second","third","fourth","fifth","sixth","seventh"]
+
+//2) Avec nos listes de restaruants, affichez en une seul ligne, pour chaque restaurant, "le restaraunt {mon_resto} a {ma_quantite} d'offres sur son menu"
+var restaruants=[{name:"My fast food",items:["Burgers","Kebab","Cookies","Beer"]},{name:"La brasserie du coin",items:["Ouef mayo","Foie gras","Entrecote","Vin rouge","vin blanc"]},{name:"La boufe orientale",items:["houmous","falafel","Schawaram","Lebaneh"]}]
+
+
+//3) Avec ce tableau d'objets, on aura une clé "animals" dans notre tableau qui sera un tableau, si la clé animals a plus de 3 elements, affichez les informations de notre objet. Affichez le nom de la classe (la clé name) et listez tous les animaux qui appartiennent a cette classe Par exemple, "liste de repitles" et en dessous tous nos reptiles !
+
+var myAnimalsObj=[{name:"Mammal",animals:["Dog","Cat","Lion","Wolf","Humans"]},{name:"Reptiles",animals:["Lizard","Snake","Crocodile","Turtles"]},{name:"Amphibians",animals:["Frog","Salamender"]},{name:"Fish",animals:["Sharks"]}];
+
+//4) Toujours avec le meme tableau, construisez un nouveau tableau et, encore une fois, si nous avons plus de 3 animaux dans notre classe (representé par la clé name), alimenter ce nouveau tableau avec l'objet actuelle qui contient plus de 3 animaux
+
+// Bonus
+ // Nous volouns que un entier qui commencera a zero attiegne une valeur supperiere ou egale a 10000 et tant que cette valeur n'a pas attient les 10,000 nous allons continuer a incrementer sa valeur par un  entier aleatoire entre 0 et 100. Attention! 10,000 en programmation ne sera pas reconnu comme un chiffre! Il faut ecrire 10000!
+
+ 
