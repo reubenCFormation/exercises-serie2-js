c04491556118a9f843117bc5c079e8e0d3cce0be
yunities-front <reubenCFormation@gmail.com>
2023-08-07 14:16:21 +0200

    modify exo-matin serie2

--- exo-matin.js
+++ exo-matin.js
@@ -1,15 +1,26 @@
 //1) Affichez, via un console.log, chaque elemnt de notre tableau de maniere dynamique (hint: Une boucle!)
  var myArray=["first","second","third","fourth","fifth","sixth","seventh","another!"];
 
-//2) Avec nos listes de restaruants, affichez en une seul ligne, pour chaque restaurant, "le restaraunt {mon_resto} a {ma_quantite} d'offres sur son menu"
-var restaruants=[{name:"My fast food",items:["Burgers","Kebab","Cookies","Beer"]},{name:"La brasserie du coin",items:["Ouef mayo","Foie gras","Entrecote","Vin rouge","vin blanc"]},{name:"La boufe orientale",items:["houmous","falafel","Schawaram","Lebaneh"]}];
+ var restaruants=[{name:"My fast food",items:["Burgers","Kebab","Cookies","Beer"],city:"Paris"},{name:"La brasserie du coin",items:["Ouef mayo","Foie gras","Entrecote","Vin rouge","vin blanc"],city:"Bordeaux"},{name:"La boufe orientale",items:["houmous","falafel","Schawaram","Lebaneh"],city:"Lyon"}];
 
+//2) Avec nos listes de restaruants, affichez en une seul ligne, pour chaque restaurant, "Le restaurant s'apelle {name} et il se trouve a {city}"
 
-//3) Avec ce tableau d'objets, on aura une clé "animals" dans notre tableau qui sera un tableau, si la clé animals a plus de 3 elements, affichez les informations de notre objet. Affichez le nom de la classe (la clé name) et listez tous les animaux qui appartiennent a cette classe Par exemple, "liste de repitles" et en dessous tous nos reptiles!
+//3) Toujours avec nos listes de restaruants, affichez en une seul ligne, pour chaque restaurant,  "Le restaurant s'apelle {name} et il a {nombre de plats} sur son menu"
 
 var myAnimalsObj=[{name:"Mammal",animals:["Dog","Cat","Lion","Wolf","Humans"]},{name:"Reptiles",animals:["Lizard","Snake","Crocodile","Turtles"]},{name:"Amphibians",animals:["Frog","Salamender"]},{name:"Fish",animals:["Sharks"]}];
 
-//4) Toujours avec le meme tableau, construisez un nouveau tableau et, encore une fois, si nous avons plus de 3 animaux dans notre classe (representé par la clé name), alimenter ce nouveau tableau avec l'objet actuelle qui contient plus de 3 animaux
+//4) En vous inspirant du tableau myAnimalsObj!
+//a) avec une boucle faites en sorte d'afficher le nom de chacun de nos animaux (Mammal,Reptiles etc) Pensez a la clé name!
+//b)Faites en sorte d'afficher chacun de nos animaux qui se trouve dans la clé animals sur chacun de nos objets
+// Bonus 1
+//5) Avec ce tableau d'objets, on aura une clé "animals" dans notre tableau qui sera un tableau, si la clé animals a plus de 3 elements, affichez les informations de notre objet. Affichez le nom de la classe (la clé name) et listez tous les animaux qui appartiennent a cette classe Par exemple, "liste de repitles" et en dessous tous nos reptiles!
+
+
+//6) Toujours avec le meme tableau (myAnimalsObj), construisez un nouveau tableau et, encore une fois, si nous avons plus de 3 animaux dans notre classe (representé par la clé name), alimenter ce nouveau tableau avec l'objet actuelle qui contient plus de 3 animaux
+
+
+
+
 
 // Bonus
  // Nous volouns que un entier qui commencera a zero attiegne une valeur supperiere ou egale a 10000 et tant que cette valeur n'a pas attient les 10,000 nous allons continuer a incrementer sa valeur par un  entier aleatoire entre 0 et 100. Attention! 10,000 en programmation ne sera pas reconnu comme un chiffre! Il faut ecrire 10000!
