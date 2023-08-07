98599d1e60b8e3e928702febd68bdc626a88e406
yunities-front <reubenCFormation@gmail.com>
2023-08-07 13:52:08 +0200

    add conditions

---
+++ conditions.js
@@ -0,0 +1,18 @@
+
+
+//1) Creer une variable que vous allez appeler number et donnez lui la valeur 200. Creer une autre variable que vous allez appeler anotherNumber et donnez lui une valeur qui sera inferieur a 200.  Creer une condition pour pouvoir tester si $number et supperieur a $anotherNumber et affichez qq chose dans un echo si c'est le cas 
+
+//2) Creer une variable que vous allez appeler myName et donnez lui la valeur que vous souhatiez. Enchainer des ifs suivi par des else if pour verifier que notre variable myName est egale a ce que vous aviez choisi et enfin suivi par un else. Faites en sorte pour faire que nous allons tomber dans le else
+
+//3) Encore une fois creer une variable newNumber et cette fois si, faites une comparaison entre newNumber et un autre numero que vous allez choisir (creer une variable). Faites en sorte d'ecrire plusiers conditions et faites en sorte que au moins deux de ces conditions seront satisfaites
+ //3a) faites en sorte que chaque fois que nous satisfaisons la condition, notre console.log sera afficher
+ //3b) ensuite faites en sortes que notre echo ne sera afficher que une fois (hint:pensez aux else if)
+
+//4) Pour chaque index de notre tableau names, tester si la longeur de notre chaine de caractere est superieur a 5 characteres. Si c'est le cas, faites un console.log de la ligne suivante "conditions rempli!,nous sommes sup a 5!". Sinon, faites un console.log de,"Conditions non rempli!"
+
+var names=["Jeremy","Tom","Reuben","Eli"];
+
+
+
+
+
